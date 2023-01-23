00cd7f1ce513552b8ca214fb2e4a48cef3c377f1
BlinkCoinn <blink182.coin@gmail.com>
2023-01-22 20:16:11 -0600

    master

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 function Header() {
-    return(
-        <header className='app-header'>
+    return (
+        <header className="border-b p-3 flex justify-between items-center">
+            <span className="font-bold">
             AppName
+            </span>
+            <Navigation/>
         </header>
     )
 }
