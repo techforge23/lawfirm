7d95befd17d35763dcbac807965ccfa1f65061e8
BlinkCoinn <blink182.coin@gmail.com>
2023-01-26 16:44:09 -0600

    fixed header allocation

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -17,13 +17,13 @@ const handleRunClick = async () => {
 };
 
 return (
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center p-20">
           <header>
             <h1 className="p-10">CSV File Processor</h1>
           </header>
           <div className="file-upload-container mx-4 my-4 flex flex-col">
               <input className="p-5" type="file" accept=".csv" onChange={handleFileUpload}/>
-              <button className="bg-black hover:bg-gray-700 text-white font-medium py-2 px-8 rounded-full"
+              <button className="bg-black hover:bg-gray-700 text-white font-medium py-2 px-2 rounded-full"
               onClick={handleRunClick}>Run</button>
           </div>
       </div>
--- src/Components/JoslynHeader.js
+++ src/Components/JoslynHeader.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const JoslynHeader = () => {
 return (
-    <div className="absolute top-0 left-0 pl-10 pt-10">
-            <img src="/JoslynLogo.png" alt="Joslyn Law Firm logo" width="536" height="168" className="w-full" />
-    </div>
-
+        <div className=" absolute top-0 h-1/5 w-full">
+            <div className="left-0 pl-10 pt-10">
+                <img src="/JoslynLogo.png" alt="Joslyn Law Firm logo" width="536" height="168"/>
+            </div>
+        </div>
     );
 }
 
