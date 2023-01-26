ff2414b4a37e32ab6493b7233b28cc32e14be17e
BlinkCoinn <blink182.coin@gmail.com>
2023-01-26 16:23:41 -0600

    added Joslyn Logo

--- src/Components/JoslynHeader.js
+++ src/Components/JoslynHeader.js
@@ -3,8 +3,7 @@ import React from 'react';
 const JoslynHeader = () => {
 return (
     <div className="absolute top-0 left-0 pl-10 pt-10">
-            <img src="/TestLogo.png" alt="Joslyn Law Firm logo" width="150" height="150" className="w-24 h-24 pl-2" />
-            <h1 className="title text-2xl font-medium pl-2">JOSLYN LAW FIRM</h1>
+            <img src="/JoslynLogo.png" alt="Joslyn Law Firm logo" width="536" height="168" className="w-full" />
     </div>
 
     );
